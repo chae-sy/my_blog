54ad6e85ef80f46ba2ac6e6e56e01ca2bb4f4636
chae-sy <sendemailtocsy@gmail.com>
2025-02-02 23:39:00 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false, // Disable fs module for the frontend
+      dns: false, // Disable dns module for the frontend
+      net: false, // Disable net module for the frontend
+      tls: false, // Disable tls module for the frontend
+    };
+    return config;
+  },
+};
