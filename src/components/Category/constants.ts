ec858d685b6c2ab245ca22ae5559c0643a842a05
chae-sy <67832005+chae-sy@users.noreply.github.com>
2025-03-03 17:36:15 +0900

    Update constants.ts

--- src/components/Category/constants.ts
+++ src/components/Category/constants.ts
@@ -6,11 +6,11 @@ export const COLOR_SET: Record<string, string> = {
   4: "rgb(191 219 254)",
   5: "rgb(204 251 241)",
   6: "rgb(187 247 208)",
-  7: "rgb(226 232 240)",
+  7: "rgb(255 212 194)",
   8: "rgb(254 249 195)",
-  9: "rgb(221 214 254)",
+  9: "rgb(255 230 185)",
   A: "rgb(254 202 202)",
-  B: "rgb(255 170 135)",
+  B: "rgb(226 232 240))",
   C: "rgb(252 231 243)",
   D: "rgb(233 213 255)",
   E: "rgb(199 210 254)",
@@ -27,11 +27,11 @@ export const COLOR_SET: Record<string, string> = {
 4: Light Blue
 5: Aqua
 6: Pale Green
-7: Light Gray
+7: Light Salmon
 8: Pale Yellow
-9: Pale Lavender (same as 0)
+9: Pastel Peach
 A: Light Red
-B: Orange 
+B: Light gray
 C: Light Pink (similar to 1)
 D: Lavender Purple
 E: Pale Blue
